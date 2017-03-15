915296552bafdf094412e09ff97623886384e4d8
Javi Perán <javperan@gmail.com>
2017-03-15 13:50:08 +0100

    Primer Commit

---
+++ app/src/main/java/com/example/javi/pruebajson/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.javi.pruebajson;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.Toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+
+            case R.id.menu_acerca:
+
+                Toast.makeText(this, "Has pulsado Acerca", Toast.LENGTH_SHORT).show();
+
+            case R.id.menu_refresh:
+
+                Toast.makeText(this, "Has pulsado Refrescar",Toast.LENGTH_SHORT).show();
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
