47e9bdda7f4e6b0b50daccb240a33e813ee132da
Javi Perán <javperan@gmail.com>
2017-03-17 09:46:44 +0100

    Chapuza

--- app/src/main/java/com/example/javi/pruebajson/MainActivity.java
+++ app/src/main/java/com/example/javi/pruebajson/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         toolbar = (Toolbar) findViewById(R.id.Toolbar);
         setSupportActionBar(toolbar);
-        toolbar.setTitleTextColor(Color.WHITE);
+
 
     }
 
